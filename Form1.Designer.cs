f4b3a4a17eeca1abc33d4bd4bb9697eb98acf531
Bas van der Wulp <bas.vanderwulp@fontys.nl>
2020-12-09 11:27:10 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace DragNDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbGraphic = new System.Windows.Forms.PictureBox();
+            this.dragItem1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGraphic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dragItem1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbGraphic
+            // 
+            this.pbGraphic.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pbGraphic.Location = new System.Drawing.Point(12, 12);
+            this.pbGraphic.Name = "pbGraphic";
+            this.pbGraphic.Size = new System.Drawing.Size(625, 391);
+            this.pbGraphic.TabIndex = 0;
+            this.pbGraphic.TabStop = false;
+            this.pbGraphic.DragDrop += new System.Windows.Forms.DragEventHandler(this.pbGraphic_DragDrop);
+            this.pbGraphic.DragEnter += new System.Windows.Forms.DragEventHandler(this.pbGraphic_DragEnter);
+            // 
+            // dragItem1
+            // 
+            this.dragItem1.Image = ((System.Drawing.Image)(resources.GetObject("dragItem1.Image")));
+            this.dragItem1.Location = new System.Drawing.Point(646, 12);
+            this.dragItem1.Name = "dragItem1";
+            this.dragItem1.Size = new System.Drawing.Size(75, 104);
+            this.dragItem1.TabIndex = 1;
+            this.dragItem1.TabStop = false;
+            this.dragItem1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dragItem1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 423);
+            this.Controls.Add(this.dragItem1);
+            this.Controls.Add(this.pbGraphic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbGraphic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dragItem1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbGraphic;
+        private System.Windows.Forms.PictureBox dragItem1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DragNDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pbGraphic.AllowDrop = true;
+        }
+
+        private void dragItem1_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragItem1.DoDragDrop(dragItem1, DragDropEffects.Copy);
+        }
+
+        private void pbGraphic_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(typeof(PictureBox)))
+            {
+                Debug.WriteLine("DragEnter triggered with an acceptable datatype!");
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                Debug.WriteLine("DragEnter triggered with no acceptable datatypes :-(");
+            }
+        }
+
+        private void pbGraphic_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(PictureBox)))
+            {
+                Debug.WriteLine("DragDrop triggered with an acceptable datatype!");
+                Image img = ((PictureBox)e.Data.GetData(typeof(PictureBox))).Image;
+                Point relLocation = new Point(e.X, e.Y);
+                Point controlLocation = this.PointToScreen(pbGraphic.Location);
+                relLocation.Offset(-controlLocation.X, -controlLocation.Y);
+                using (Graphics g = pbGraphic.CreateGraphics())
+                {
+                    g.DrawImageUnscaled(img, relLocation);
+                }
+            }
+            else
+            {
+                Debug.WriteLine("DragDrop triggered with no acceptable datatypes :-(");
+            }
+        }
+    }
+}
